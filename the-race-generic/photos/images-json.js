bec8bae07f3542e1e5c85feaa87ccbe572035579
mador-till-prod <61970359+mador-till-prod@users.noreply.github.com>
2021-07-14 16:21:52 +0300

    Add files via upload

---
+++ the-race-generic/photos/images-json.js
@@ -0,0 +1,56 @@
+// מערך התמונות
+var images = [{
+        "src": "../assets/images/picRight.svg", // קיושר לקובץ התמונה
+        "answer": "right", // האם התשובה נכונה או לא נכונה: right/wrong
+        "feedback": "נכון מאוד", // כותרת לחיווי
+        "feedbackExplenation": "הסבר למה התשובה נכונה" // הסבר למה זה נכון או לא נכון
+    },
+    {
+        "src": "../assets/images/picWrong.svg",
+        "answer": "wrong",
+        "feedback": "לא ממש...",
+        "feedbackExplenation": "הסבר למה התשובה לא נכונה"
+    },
+    {
+        "src": "../assets/images/picWrong.svg",
+        "answer": "wrong",
+        "feedback": "לא ממש...",
+        "feedbackExplenation": "הסבר למה התשובה לא נכונה"
+    },
+    {
+        "src": "../assets/images/picWrong.svg",
+        "answer": "wrong",
+        "feedback": "לא ממש...",
+        "feedbackExplenation": "הסבר למה התשובה לא נכונה"
+    },
+    {
+        "src": "../assets/images/picRight.svg",
+        "answer": "right",
+        "feedback": "נכון מאוד!",
+        "feedbackExplenation": "הסבר למה התשובה נכונה"
+    },
+    {
+        "src": "../assets/images/picWrong.svg",
+        "answer": "wrong",
+        "feedback": "לא ממש..",
+        "feedbackExplenation": "הסבר למה התשובה לא נכונה"
+    },
+    {
+        "src": "../assets/images/picWrong.svg",
+        "answer": "wrong",
+        "feedback": "לא ממש..",
+        "feedbackExplenation": "הסבר למה התשובה לא נכונה"
+    },
+    {
+        "src": "../assets/images/picWrong.svg",
+        "answer": "wrong",
+        "feedback": "לא ממש..",
+        "feedbackExplenation": "הסבר למה התשובה לא נכונה"
+    },
+    {
+        "src": "../assets/images/picRight.svg",
+        "answer": "right",
+        "feedback": "נכון מאוד!",
+        "feedbackExplenation": "הסבר למה התשובה נכונה"
+    },
+];
